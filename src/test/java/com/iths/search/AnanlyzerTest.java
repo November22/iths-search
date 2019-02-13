8399c6c8c29416c5cec4f2c980d967140b1fc380
sen.huang <sen.huang@yeepay.com>
2019-02-13 22:58:32 +0800

    search——中文分词器SmartChineseAnalyzer添加停用词

--- src/test/java/com/iths/search/AnanlyzerTest.java
+++ src/test/java/com/iths/search/AnanlyzerTest.java
@@ -1,6 +1,7 @@
 package com.iths.search;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.CharArraySet;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import org.wltea.analyzer.lucene.IKAnalyzer;
 
 import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * 要看分析器的分析效果，只需要看TokenStream中的内容就可以了。
@@ -53,10 +55,30 @@ public class AnanlyzerTest {
         tokenStream.close();
     }
 
+    /**
+     * SmartChineseAnalyzer
+     * @throws IOException
+     */
     @Test
     public void testSmartcn() throws IOException{
-        /* 1、创建一个分析器对象 */
-        Analyzer analyzer = new SmartChineseAnalyzer(); // 官方推荐的标准分析器
+
+        //添加停用词
+        CharArraySet cas = new CharArraySet( 0, true);
+        // 自定义停用词
+        String[] self_stop_words = { "的", "了", "呢", "，", "0", "：", ",", "是", "流" };
+        for (int i = 0; i < self_stop_words.length; i++) {
+            cas.add(self_stop_words[i]);
+        }
+        // 加入系统默认停用词
+        Iterator<Object> itor = SmartChineseAnalyzer.getDefaultStopSet().iterator();
+        while (itor.hasNext()) {
+            cas.add(itor.next());
+        }
+
+        //1、创建一个分析器对象
+        //如果使用 stopwords.txt 那必须在包下面操作
+        Analyzer analyzer = new SmartChineseAnalyzer(false); // 官方推荐的标准分析器
+
         // 2、从分析器对象中获得tokenStream对象
         // 参数1：域的名称，可以为null，或者是""
         // 参数2：要分析的文本
@@ -64,7 +86,7 @@ public class AnanlyzerTest {
         // 3、设置一个引用(相当于指针)，这个引用可以是多种类型，可以是关键词的引用，偏移量的引用等等
         // charTermAttribute 对象代表当前的关键词
         CharTermAttribute charTermAttribute = tokenStream.addAttribute(CharTermAttribute.class);
-        // 偏移量(其实就是关键词在文档中出现的位置，拿到这个位置有什么用呢？因为我们将来可能要对该关键词进行高亮显示，进行高亮显示要知道这个关键词在哪？)
+        // 偏移量(其实就是关键词在文档中出现的位置，我们将来可能要对该关键词进行高亮显示，进行高亮显示要知道这个关键词在哪)
         OffsetAttribute offsetAttribute = tokenStream.addAttribute(OffsetAttribute.class);
 
         KeywordAttribute keywordAttribute = tokenStream.addAttribute(KeywordAttribute.class);
@@ -78,13 +100,15 @@ public class AnanlyzerTest {
             // 6、打印单词
             System.out.println(charTermAttribute);
             System.out.println("end→" + offsetAttribute.endOffset()); // 关键词结束位置
-            System.out.println("keywordAttribute->"+keywordAttribute.isKeyword());
-            System.out.println("typeAttribute->"+typeAttribute.type());
         }
         // 7、关闭tokenStream对象
         tokenStream.close();;
     }
 
+    /**
+     * 版本问题
+     * @throws IOException
+     */
     @Test
     public void testIkAnalyzer() throws IOException{
         // 1、创建一个分析器对象
